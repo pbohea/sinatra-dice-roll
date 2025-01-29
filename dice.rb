72e209b4477a67cf4c24729e5c36a029649896ae
pbohea <pbohea@gmail.com>
2025-01-29 16:46:19 +0000

    couple dice rolls

--- dice.rb
+++ dice.rb
@@ -1,6 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/zebra") do
-  "Hello world"
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
 end
