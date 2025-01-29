f9f05306e39fa56168ebcb766dbe070b4a864a49
pbohea <pbohea@gmail.com>
2025-01-29 21:37:18 +0000

    made ERB

--- dice.rb
+++ dice.rb
@@ -7,6 +7,10 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+get("/") do
+erb(:elephant)
+end
+
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
@@ -15,7 +19,9 @@ get("/dice/2/6") do
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
   "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  <p>#{outcome}</p>
+  "
+
 
 end
 
